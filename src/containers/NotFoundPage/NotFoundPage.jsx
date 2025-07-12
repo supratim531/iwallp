89594752c97c8808fc5fde379cc5a6885825ab2a
arpanghosh2416 <arpanghosh2416@gmail.com>
2025-07-12 18:16:06 +0530

    Not found page done to some extent

--- src/containers/NotFoundPage/NotFoundPage.jsx
+++ src/containers/NotFoundPage/NotFoundPage.jsx
@@ -3,8 +3,26 @@ import React from "react";
 const NotFoundPage = (props) => {
   return (
     <section>
-      <h2>Page Not Found</h2>
-      <p>No page found at this path.</p>
+      <div class="bg-gradient-to-r from-purple-300 to-blue-200">
+        <div class="m-auto flex min-h-screen w-9/12 items-center justify-center py-16">
+          <div class="overflow-hidden bg-white pb-8 shadow sm:rounded-lg">
+            <div class="border-t border-gray-200 pt-8 text-center">
+              <h1 class="text-9xl font-bold text-purple-400">404</h1>
+              <h1 class="py-8 text-6xl font-medium">oops! Page not found</h1>
+              <p class="px-12 pb-8 text-2xl font-medium">
+                Oops! The page you are looking for does not exist. It might have
+                been moved or deleted.
+              </p>
+              <button class="mr-6 rounded-md bg-gradient-to-r from-purple-400 to-blue-500 px-6 py-3 font-semibold text-white hover:from-pink-500 hover:to-orange-500">
+                HOME
+              </button>
+              <button class="rounded-md bg-gradient-to-r from-red-400 to-red-500 px-6 py-3 font-semibold text-white hover:from-red-500 hover:to-red-500">
+                Contact Us
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
